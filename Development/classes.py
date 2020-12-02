a06eb14185579d81dade7a3a99a5c7b57c9e326b
Olli Appelt <30849@ma-web.nl>
2020-12-02 09:28:17 +0000

    Python main.py

---
+++ Development/classes.py
@@ -0,0 +1,23 @@
+
+class Mario :
+
+    _lives = 3
+    _speed = 30.0
+
+    def Test(self):
+        print("Hallo")
+        print("Speed is: ", self._speed)
+    
+    def __init__(self):
+        Instructies
+
+
+instanceMario = Mario()
+nogEenMario = Mario()
+
+print( instanceMario._lives )
+instanceMario.Test()
+instanceMario._speed = 50.5
+
+print("instanceMario snelheid:", instanceMario._speed)
+print("nogEenMario snelheid:", nogEenMario._speed)
